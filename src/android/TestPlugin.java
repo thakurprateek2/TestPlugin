bde0bf33e7ae1127422d06ceb9ff908422ad62e6
thakurprateek2 <thakurprateek2@gmail.com>
2014-04-08 01:15:07 +0530

    TestPlugin temp change

--- src/android/TestPlugin.java
+++ src/android/TestPlugin.java
@@ -1,25 +1,30 @@
 package com.phonegap.helloworld;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 
 public class TestPlugin extends CordovaPlugin {
 	 public static final String ACTION_TEST_PLUGIN_ENTRY = "testPluginEntry";
-	 HelloWorld hw = new HelloWorld(); 
+	 public static CallbackContext cc; 
 	 
 	 @Override
 	 public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		 try {
 			 	System.out.println(":::Test Plugin from JS "+action);
 			    if (ACTION_TEST_PLUGIN_ENTRY.equals(action)) { 
-			    	hw.testFunc(callbackContext);
+			    	cc = callbackContext;
+			    	testFunc();
+//			    	callbackContext.success();
 			    }
-			    callbackContext.error("Invalid action");
+//			    callbackContext.error("Invalid action");
 			    return false;
 			} catch(Exception e) {
 			    System.err.println("Test Plugin Exception: " + e.getMessage());
@@ -28,4 +33,31 @@ public class TestPlugin extends CordovaPlugin {
 			} 
 	 }
 	 
+	    public void testFunc(){
+		   	System.out.println(":::testFunc");
+		   	
+		   	final Intent intent = new Intent();
+		    intent.putExtra("result","good");
+		   	ComponentName cName = new ComponentName
+		   	("com.idmission.apitservice","com.idmission.apitservicelib.web.TestActivity");
+
+		   	intent.setComponent(cName);         
+//		   	startActivity(intent);
+		   	
+//		    Intent i = new Intent(Intent.ACTION_SEND, Uri.parse("content://contacts"));
+//		    i.setType(HTTP.PLAIN_TEXT_TYPE);
+//		   	Intent i = new Intent("com.sample.action.MY_CUSTOM_ACTION");
+//		    System.out.println(":::testFunc");
+//		    PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+//		    System.out.println(":::testFunc");
+//		    List<ResolveInfo> activities = packageManager.queryIntentActivities(i, 0);
+//		    System.out.println(":::testFunc");
+//		    boolean isIntentSafe = activities.size() > 0;
+//		    System.out.println(":::testFunc");
+//		    if(isIntentSafe){
+		    	System.out.println(":::IntentSafe");
+		    	this.cordova.setActivityResultCallback(TestPlugin.this); 
+		    	this.cordova.getActivity().startActivityForResult(intent, 101);
+//		    }
+		 }
 }
